3a8e3d145a23d3735fc97c3f848e5ef20dadb091
Steve Billings <billings.steve@gmail.com>
2023-02-24 12:20:16 -0500

    fix test

--- src/loiterbot/tests/statehandler/state_handler_just_go_test.cpp
+++ src/loiterbot/tests/statehandler/state_handler_just_go_test.cpp
@@ -6,7 +6,7 @@
 TEST(StateHandlerJustGoTest, StraightAhead)
 {
   LaserCharacteristics laser_characteristics = LaserCharacteristics(LASER_ANGLE_MINIMUM, LASER_ANGLE_INCREMENT, 4ul, 2ul);
-  LaserAnalysis laser_analysis = LaserAnalysis(true, false, false, 0.0, 4.0l);
+  LaserAnalysis laser_analysis = LaserAnalysis(true, false, false, 0.0, 2.0l);
   StateHandlerJustGo state_handler = StateHandlerJustGo();
   History history = History();
   history.set_obstacle_last_seen_time(1000.0l, true);
@@ -17,15 +17,14 @@ TEST(StateHandlerJustGoTest, StraightAhead)
   EXPECT_EQ(action.get_state(), State::JUST_GO);
   // Ensure the values are reasonable without being overly sensitive to magnitude
   EXPECT_TRUE(action.get_velocity().has_value());
-  EXPECT_TRUE(action.get_velocity().value().get_forward() > 2.0l);
+  EXPECT_TRUE(action.get_velocity().value().get_forward() > 0.5l);
   EXPECT_NEAR(action.get_velocity().value().get_yaw(), 0.0, 0.01l);
-
 }
 
 TEST(StateHandlerJustGoTest, AheadRight)
 {
   LaserCharacteristics laser_characteristics = LaserCharacteristics(LASER_ANGLE_MINIMUM, LASER_ANGLE_INCREMENT, 4ul, 2ul);
-  LaserAnalysis laser_analysis = LaserAnalysis(true, false, false, -1*M_PI/4.0, 4.0l);
+  LaserAnalysis laser_analysis = LaserAnalysis(true, false, false, -1*M_PI/4.0, 2.0l);
   StateHandlerJustGo state_handler = StateHandlerJustGo();
   History history = History();
   history.set_obstacle_last_seen_time(1000.0l, true);
@@ -36,27 +35,13 @@ TEST(StateHandlerJustGoTest, AheadRight)
   EXPECT_EQ(action.get_state(), State::JUST_GO);
   // Ensure the values are reasonable without being overly sensitive to magnitude
   EXPECT_TRUE(action.get_velocity().has_value());
-  EXPECT_TRUE(action.get_velocity().value().get_forward() > 2.0l);
+  EXPECT_TRUE(action.get_velocity().value().get_forward() > 0.5l);
   EXPECT_TRUE(action.get_velocity().value().get_forward() < 8.0l);
   EXPECT_NEAR(action.get_velocity().value().get_yaw(), 0.5, 0.1l);
-
 }
-TEST(StateHandlerJustGoTest, Temp)
+
+int main(int argc, char ** argv)
 {
-  LaserCharacteristics laser_characteristics = LaserCharacteristics(LASER_ANGLE_MINIMUM, LASER_ANGLE_INCREMENT, 4ul, 2ul);
-  LaserAnalysis laser_analysis = LaserAnalysis(true, false, false, 0.004370, 0.200001l);
-  StateHandlerJustGo state_handler = StateHandlerJustGo();
-  History history = History();
-  history.set_obstacle_last_seen_time(1000.0l, true);
-  history.set_time_lost(0.1l);
-
-  Action action = state_handler.act(history, 0.0l, laser_characteristics, laser_analysis);
-
-  EXPECT_EQ(action.get_state(), State::JUST_GO);
-  // Ensure the values are reasonable without being overly sensitive to magnitude
-  EXPECT_TRUE(action.get_velocity().has_value());
-  EXPECT_TRUE(action.get_velocity().value().get_forward() > 2.0l);
-  EXPECT_TRUE(action.get_velocity().value().get_forward() < 8.0l);
-  EXPECT_NEAR(action.get_velocity().value().get_yaw(), 0.5, 0.1l);
-
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
