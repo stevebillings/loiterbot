46c14c5bff6946780d08a6ff722275b9cda3cebf
Steve Billings <billings.steve@gmail.com>
2023-03-02 16:41:02 -0500

    two simulator worlds, diff levels of difficulty

--- scripts/start_difficult.sh
+++ scripts/start_difficult.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ign gazebo -v 3 -r ./gazeboworlds/obstacle_box.sdf &
+ign gazebo -v 3 -r ./gazeboworlds/obstacle_box_difficult.sdf &
 sleep 5
 ros2 run ros_ign_bridge parameter_bridge /lidar@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan --ros-args -r /lidar:=/laser_scan &
 ros2 run ros_ign_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist &
